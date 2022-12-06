f044a2757e39878bd388000214b4e6b7920784da
Robbo270 <116882868+Robbo270@users.noreply.github.com>
2022-12-06 10:59:38 +0000

    Add files via upload

---
+++ p5bubbles-master/js/sketch.js
@@ -0,0 +1,29 @@
+
+function setup() {
+    var canvas = createCanvas(800, 800);
+    canvas.parent("p5container");   
+    background(0);
+    colorMode(HSB);
+
+    for(var i = 0; i < 500; i++){
+        circles();  
+    }
+
+}
+
+function draw() {
+
+}
+
+function circles() {
+
+    var x = random(width);
+    var y = random (height);
+    var d = random(5, 150);
+    var hue = random(120, 255)
+
+    noStroke();
+
+    fill(hue, 255, 255, 0.1);
+    circle(x, y, d);
+}
